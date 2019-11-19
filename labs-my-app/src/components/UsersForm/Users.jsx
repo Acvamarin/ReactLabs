aa79c5cbbc4d65ef4abf7f3e26e695ff5de19338
Acvamarin <makavchik.kirill@mail.ru>
2019-11-19 22:55:22 +0300

    redux-rect form update

--- labs-my-app/src/components/UsersForm/Users.jsx
+++ labs-my-app/src/components/UsersForm/Users.jsx
@@ -4,21 +4,25 @@ const Users = props => {
   let onRemoveUser = id => {
     props.RemoveUser(id);
   };
-  let messagesElements = props.usersData.map(el => {
+  let messagesElements = props.usersData.map((el, i) => {
     return (
-      <div className={s.item}>
-        <img src="https://oboi.ws/wallpapers/27_11168_oboi_surovyj_paren_s_sigaroj_2560x2048.jpg"></img>
-        {el.name}
-        {el.lastName}
-        {el.email}
-        <button onClick={() => onRemoveUser(el.id)}>Remove</button>
-      </div>
+      <tr key={i}>
+        <td>{el.name}</td>
+        <td>{el.lastName}</td>
+        <td>{el.email}</td>
+        <td>
+          <div className={s.form}>
+            <button onClick={() => onRemoveUser(el.id)}>Remove</button>
+          </div>
+        </td>
+      </tr>
     );
   });
   let textElements = React.createRef();
   let textElements1 = React.createRef();
   let textElements2 = React.createRef();
-  let onAddUser = () => {
+  let onAddUser = e => {
+    e.preventDefault();
     props.AddUser();
   };
 
@@ -30,31 +34,63 @@ const Users = props => {
     props.updateNewUserText(text, text1, text2);
   };
   return (
-    <div>
-      <h4>add Users </h4>
+    <div className={s.container}>
       <div>
-        <div>
-          <textarea
-            onChange={onTextChange}
-            ref={textElements}
-            value={props.newText}
-          />
-          <textarea
-            onChange={onTextChange}
-            ref={textElements1}
-            value={props.newText1}
-          />
-          <textarea
-            onChange={onTextChange}
-            ref={textElements2}
-            value={props.newText2}
-          />
-        </div>
-        <div>
-          <button onClick={onAddUser}>Add post</button>
-        </div>
+      <h1> Form add Users </h1>
       </div>
-      <div className={s.users}>{messagesElements}</div>
+
+      <form>
+        <div className={s.form}>
+          <div className={s.block}>
+            <div className={s.item}>
+              <div>
+                <label>Name</label>
+              </div>
+              <textarea
+                onChange={onTextChange}
+                ref={textElements}
+                value={props.newText}
+              />
+            </div>
+            <div className={s.item}>
+              <div>
+                <label>lastName</label>
+              </div>
+              <textarea
+                onChange={onTextChange}
+                ref={textElements1}
+                value={props.newText1}
+              />
+            </div>
+            <div className={s.item}>
+              <div>
+                <label>email</label>
+              </div>
+              <textarea
+                onChange={onTextChange}
+                ref={textElements2}
+                value={props.newText2}
+              />
+            </div>
+
+            <button onClick={onAddUser}>Add User</button>
+          </div>
+        </div>
+      </form>
+      <div className={s.table}>
+        <table>
+          <thead>
+            <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>{messagesElements}</tbody>
+        </table>
+      </div>
     </div>
   );
 };
